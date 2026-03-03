0f477492ae61085dfb513f6c9ef6be0db58215c5
rafatusa <piealrafat@gmail.com>
2026-03-03 14:08:25 -0500

    Auto-fix attempt 2: terraform/outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,26 +1,26 @@
 output "instance_id" {
   description = "The ID of the EC2 instance running nginx"
-  value       = aws_instance.web_app3_nginx.id
+  value       = aws_instance.web_app.id
 }
 
 output "instance_public_ip" {
   description = "The public IP address of the EC2 instance"
-  value       = aws_instance.web_app3_nginx.public_ip
+  value       = aws_instance.web_app.public_ip
 }
 
 output "instance_private_ip" {
   description = "The private IP address of the EC2 instance"
-  value       = aws_instance.web_app3_nginx.private_ip
+  value       = aws_instance.web_app.private_ip
 }
 
 output "instance_public_dns" {
   description = "The public DNS name of the EC2 instance"
-  value       = aws_instance.web_app3_nginx.public_dns
+  value       = aws_instance.web_app.public_dns
 }
 
 output "nginx_url" {
   description = "The URL to access the nginx web server"
-  value       = "http://${aws_instance.web_app3_nginx.public_ip}"
+  value       = "http://${aws_instance.web_app.public_ip}"
 }
 
 output "security_group_id" {
@@ -30,20 +30,20 @@ output "security_group_id" {
 
 output "instance_state" {
   description = "The current state of the EC2 instance"
-  value       = aws_instance.web_app3_nginx.instance_state
+  value       = aws_instance.web_app.instance_state
 }
 
 output "availability_zone" {
   description = "The availability zone where the EC2 instance is deployed"
-  value       = aws_instance.web_app3_nginx.availability_zone
+  value       = aws_instance.web_app.availability_zone
 }
 
 output "ami_id" {
   description = "The AMI ID used for the EC2 instance"
-  value       = aws_instance.web_app3_nginx.ami
+  value       = aws_instance.web_app.ami
 }
 
 output "instance_type" {
   description = "The instance type of the EC2 instance"
-  value       = aws_instance.web_app3_nginx.instance_type
+  value       = aws_instance.web_app.instance_type
 }
